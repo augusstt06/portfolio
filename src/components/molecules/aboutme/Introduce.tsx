e46d25b06e9339738de6e6c5f22fdd45762e3e39
augusstt06 <augusstt06@gmail.com>
2024-09-25 00:18:06 +0900

    fix: modify introduce

--- src/components/molecules/aboutme/Introduce.tsx
+++ src/components/molecules/aboutme/Introduce.tsx
@@ -1,3 +1,9 @@
+import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa'
+import { SiGitbook } from 'react-icons/si'
+
+import { BLOG, GIT } from '@/constant'
+
 export default function Introduce() {
   const paragraphs = [
     {
@@ -22,24 +28,48 @@ export default function Introduce() {
     {
       key: 'description',
       content: (
-        <p>
-          기능완성에서 나아가 재사용/유지보수 개선, 코드 품질 향상을 위해
-          지속적으로 코드를 리뷰하며 <br />
-          <span className={`text-[var(--main-color)] text-3xl`}>
-            어제보다 발전한 코드
-          </span>
-          를 작성하기 위해 노력하고 있습니다 .
-        </p>
+        <div>
+          <p>
+            기능완성에서 나아가 재사용/유지보수 개선, 코드 품질 향상을 위해
+            지속적으로 코드를 리뷰하며{' '}
+          </p>
+          <p className="mb-2">
+            <span className={`text-[var(--main-color)] text-3xl`}>
+              어제보다 발전한 코드
+            </span>
+            를 작성하기 위해 노력하고 있습니다 .
+          </p>
+
+          <p>
+            또한 스터디를 진행하며 다루거나 개인 공부한 내용들을 정리해 꾸준히
+            블로그에 올리고 있으며, 수정사항 및 내용 숙지를 위해 주기적으로
+            확인하고 있습니다.
+          </p>
+        </div>
       ),
     },
   ]
 
+  const archivings = [
+    { title: 'Git', url: GIT, icon: <FaGithub /> },
+    { title: 'Blog', url: BLOG, icon: <SiGitbook /> },
+  ]
   return (
-    <article className="h-40 lg:h-auto lg:col-span-2 rounded-lg p-3 korean-font text-2xl transition-all duration-[1.5s] ease-in-out space-y-3 relative">
-      <div className="xl:top-16 md:top-12 md:left-20 lg:left-4 xl:left-24 xxl:left-[25rem] left-12 top-12 absolute text-left">
+    <article className="h-40 lg:h-auto lg:col-span-2 rounded-lg p-3 text-2xl transition-all duration-[1.5s] ease-in-out space-y-3 relative">
+      <div className="xl:top-12 md:top-12 md:left-20 lg:left-4 xl:left-24 xxl:left-[25rem] left-12 top-12 absolute text-left korean-font">
         {paragraphs.map((data) => (
           <div key={data.key}>{data.content}</div>
         ))}
+        <div className="row-flex w-auto space-x-4 mt-4">
+          {archivings.map((data) => (
+            <Link href={data.url} key={data.title}>
+              <div className="border-2 w-24 place-content-center place-items-center grid grid-cols-3 rounded-lg px-1 hover:text-black simple-transition cursor-pointer hover:bg-white">
+                <div className="col-span-1">{data.icon}</div>
+                <p className="text-md col-span-2">{data.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </article>
   )
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,5 +3,8 @@ export const ABOUTME = 'About Me'
 export const CONTACT = 'Contact'
 export const PROJECT = 'Project'
 
+export const GIT = 'https://github.com/augusstt06'
+export const BLOG = 'https://augusstt-note.gitbook.io/aug-note/'
+
 export const GIT_PORTFOLIO = 'https://github.com/augusstt06/portfolio'
 export const GIT_WEATHER = 'https://github.com/augusstt06/weatherAi'
