cad472322bd0d9c77f40ea03c38846c02c6e29be
augusstt06 <augusstt06@gmail.com>
2024-09-26 16:43:08 +0900

    fix: modify css

--- src/components/template/Contact.tsx
+++ src/components/template/Contact.tsx
@@ -3,7 +3,9 @@ import { CONTACT } from '@/constant'
 export default function Contact() {
   return (
     <section id={CONTACT} className="h-screen pt-20 grid grid-rows-4">
-      Contact
+      <h1>Contact</h1>
+      {/* FIXME: 깃허브, 메일, 전화번호 */}
+      {/* QR 코드? */}
     </section>
   )
 }
