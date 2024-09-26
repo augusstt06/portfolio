78dec87ad5b6c0c4622ba1ec27943fe5b8da1afc
augusstt06 <augusstt06@gmail.com>
2024-09-26 19:39:27 +0900

    fix: contact

--- src/components/molecules/animate/DropDownText.tsx
+++ src/components/molecules/animate/DropDownText.tsx
@@ -2,17 +2,14 @@ import { useEffect, useRef } from 'react'
 
 import gsap from 'gsap'
 
-import { useStepStore } from '@/app/store'
 import {
   addExitAnimation,
   animateHeaderText,
   animateSubParagraph,
-  setupClickHandler,
   wrapTextInSpan,
 } from '@/utils/animation/drop-down'
 
 export default function DropDownText() {
-  const { setStep } = useStepStore()
   const headParagraphRef = useRef<HTMLParagraphElement>(null)
   const subParagraphRef = useRef<HTMLParagraphElement>(null)
 
@@ -30,31 +27,33 @@ export default function DropDownText() {
     animateHeaderText(h1, small, tl)
     animateSubParagraph(subParagraphRef.current, tl)
     addExitAnimation(h1, small, subParagraphRef.current, tl)
-    setupClickHandler(subParagraphRef.current, tl, setStep)
 
     return () => {
       subParagraphRef.current?.removeEventListener('click', () => {
         tl.play()
       })
     }
-  }, [setStep])
+  }, [])
 
   return (
     <article className="justify-center col-flex abs-center space-y-5">
+      {/* FIXME: 넓이 조절 */}
       <div
         ref={headParagraphRef}
-        className="cursor-pointer text-3xl max-w-2xl mx-auto leading-tight text-center text-[#9DF3C4]"
+        className="cursor-pointer text-2xl max-w-2/3 mx-auto leading-tight text-center text-black"
       >
-        <h1 className="mb-6 text-5xl">Hi I&apos;m Chung Yeon Kim</h1>
-        <small className="text-4xl">FrontEnd Developer</small>
+        <h1 className="mb-6 text-5xl">Passionately Developing</h1>
+        <small className="text-4xl text-[var(--main-color)]">
+          and Growing Together.
+        </small>
       </div>
-      <p
+      {/* <p
         ref={subParagraphRef}
-        className="group text-center text-white text-lg opacity-0 mt-4 cursor-pointer hover:text-[#9DF3c4] simple-transition relative px-6 py-2"
+        className="group text-center text-white text-lg opacity-0 mt-4 cursor-pointer hover:text-[var(--main-color)] simple-transition relative px-6 py-2"
       >
         Click to continue Viewing the Portfolio
-        <span className="absolute inset-0 border-2 border-[#9DF3c4] transition-all duration-700 transform scale-x-0 origin-left group-hover:scale-x-100 pointer-events-none z-[-1] rounded-xl" />
-      </p>
+        <span className="absolute inset-0 border-2 border-[var(--main-color)] transition-all duration-700 transform scale-x-0 origin-left group-hover:scale-x-100 pointer-events-none z-[-1] rounded-xl" />
+      </p> */}
     </article>
   )
 }
--- src/components/template/Contact.tsx
+++ src/components/template/Contact.tsx
@@ -1,9 +1,12 @@
 import { CONTACT } from '@/constant'
 
+import DropDownText from '@/components/molecules/animate/DropDownText'
+
 export default function Contact() {
   return (
-    <section id={CONTACT} className="h-screen pt-20 grid grid-rows-4">
+    <section id={CONTACT} className="h-screen pt-20 grid grid-rows-4 relative">
       <h1>Contact</h1>
+      <DropDownText />
       {/* FIXME: 깃허브, 메일, 전화번호 */}
       {/* QR 코드? */}
     </section>
