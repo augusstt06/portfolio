c4dd452b5cd8e0a6da4208f9bdc99a0801fc8ad7
augusstt06 <augusstt06@gmail.com>
2024-09-27 15:04:05 +0900

    fix: connect link

--- src/components/template/Contact.tsx
+++ src/components/template/Contact.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
+import Link from 'next/link'
 import { BiLogoGmail } from 'react-icons/bi'
 import { FaGithubSquare } from 'react-icons/fa'
 
-import { CONTACT } from '@/constant'
+import { CONTACT, GIT, MAIL } from '@/constant'
 
 import FadeInText from '@/components/molecules/animate/FadeInText'
 
@@ -16,9 +17,15 @@ export default function Contact() {
   const handleResetAnimation = () => {
     setIsAnimationEnd(false)
   }
+  const handleConnectEmail = useCallback(() => {
+    const gmailComposeUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(MAIL)}`
+
+    window.open(gmailComposeUrl, '_blank')
+  }, [])
+
   return (
     <section id={CONTACT} className="relative grid h-screen grid-rows-4 pt-20">
-      <article className="left-1/2 top-1/3 absolute h-full -translate-x-1/2 bg-red-500">
+      <article className="left-1/2 top-1/3 absolute h-2/3 -translate-x-1/2 space-y-12">
         <FadeInText
           text="develop and grow passionately"
           className=" text-5xl text-center text-[var(--main-color)] uppercase"
@@ -30,12 +37,18 @@ export default function Contact() {
           className={`${isAnimationEnd ? 'opacity-100' : 'opacity-0'} text-black col-flex justify-around w-[40rem] mx-auto simple-transition space-y-6`}
         >
           <div className="row-flex justify-center w-full gap-16">
-            <FaGithubSquare className="w-20 h-20 hover:scale-[1.2] simple-transition cursor-pointer" />
-            <BiLogoGmail className="w-20 h-20 hover:scale-[1.2] simple-transition cursor-pointer" />
+            <Link href={GIT}>
+              <FaGithubSquare className="w-20 h-20 hover:scale-[1.2] simple-transition cursor-pointer" />
+            </Link>
+
+            <BiLogoGmail
+              className="w-20 h-20 hover:scale-[1.2] simple-transition cursor-pointer"
+              onClick={handleConnectEmail}
+            />
           </div>
           <div className="w-full text-center bg-red-100">
             <p className="w-full text-4xl">Thank you for visit</p>
-            <p className="text-sm">Copyright 2024 ChungYeonKim - aug</p>
+            <p className="text-xs">Copyright 2024 ChungYeonKim</p>
           </div>
         </div>
       </article>
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -5,6 +5,7 @@ export const PROJECT = 'Project'
 
 export const GIT = 'https://github.com/augusstt06'
 export const BLOG = 'https://augusstt-note.gitbook.io/aug-note/'
+export const MAIL = 'augusstt06@gmail.com'
 
 export const GIT_PORTFOLIO = 'https://github.com/augusstt06/portfolio'
 export const GIT_WEATHER = 'https://github.com/augusstt06/weatherAi'
